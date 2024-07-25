afa7000017d42992651f8addb648fe9117234d0f
GitPythZ <yardbirds1957@gmail.com>
2024-07-25 13:58:00 +0300

    Update Homework3.py

--- Homework3.py
+++ Homework3.py
@@ -4,6 +4,6 @@ Age = 25
 print(Age)
 New_Age = Age + 1
 print(New_Age)
-Is_student = True
-print(Is_student)
-print(f'Name: {name} \nAge: {Age} \nNew Age: {New_Age} \nIs Student: {Is_student}')
+is_student = True
+print(is_student)
+print(f'Name: {name} \nAge: {Age} \nNew Age: {New_Age} \nIs Student: {is_student}')
