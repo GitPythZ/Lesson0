9dd059f9b140d09c4ca41c0ef8638afa5410ced5
GitPythZ <yardbirds1957@gmail.com>
2024-07-30 01:33:34 +0300

    Add files via upload

---
+++ module_2_2.py
@@ -0,0 +1,9 @@
+first = 10
+second = 9 + 1
+third = 100 - 90
+if first == second == third:
+    print("3")
+elif first == second or first == third:
+    print("2")
+elif first != second != third:
+    print("0")
