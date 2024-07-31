3fbd865d1f813b019d2b894d471ec8601ad29f26
GitPythZ <yardbirds1957@gmail.com>
2024-07-31 13:25:15 +0300

    Add files via upload

--- module_2_2.py
+++ module_2_2.py
@@ -1,9 +1,11 @@
-first = 10
-second = 9 + 1
-third = 100 - 90
+first = int(input("Введите целое число: "))
+second = int(input("Введите целое число: "))
+third = int(input("Введите целое число: "))
 if first == second == third:
     print("3")
-elif first == second or first == third:
+elif first==second!=third or first==third!=second or second==third!=first :
     print("2")
-elif first != second != third:
+elif first!=second!=third: #перепроверка.
+    print("0")
+else:
     print("0")
